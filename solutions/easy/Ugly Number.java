9dd585ae2f4cfbdca7562b694000921b84f0c1e2
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-09-02 19:57:49 +0530

    Add solution for Ugly Number

---
+++ solutions/easy/Ugly Number.java
@@ -0,0 +1,17 @@
+// Title: Ugly Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/ugly-number/
+
+        
+        for(int i=2; i<=5; i++){
+            while(num % i == 0){
+        }
+                num /= i;
+            }
+        return num == 1;
+        }
+        if(num <= 0){
+            return false;
+    public boolean isUgly(int num) {
+class Solution {
