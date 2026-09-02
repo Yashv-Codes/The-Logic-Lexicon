d985c85341f472adb1c87af009dc223ce0d5b7fa
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-09-02 18:39:22 +0530

    Add solution for Happy Number

---
+++ solutions/easy/Happy Number.java
@@ -0,0 +1,17 @@
+// Title: Happy Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/happy-number/
+
+        }
+            n = sum;
+            }
+                n /= 10;
+                sum += ld*ld;
+                int ld = n % 10;
+            while(n != 0){
+        n = Math.abs(n);
+        while(n != 1 && n != 4){
+            int sum = 0;
+class Solution {
+    public boolean isHappy(int n) {
