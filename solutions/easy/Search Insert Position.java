e9a0fe1b05648fd62ff2e3024f353fc067bdb136
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-09-05 21:04:41 +0530

    Add solution for Search Insert Position

---
+++ solutions/easy/Search Insert Position.java
@@ -0,0 +1,17 @@
+// Title: Search Insert Position
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/search-insert-position/
+
+            else if(target < nums[mid]){
+                end = mid-1;
+            }
+            else{
+            }
+            if(target == nums[mid]){
+                return mid;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+        int start = 0, end = nums.length-1;
+    public int searchInsert(int[] nums, int target) {
+class Solution {
