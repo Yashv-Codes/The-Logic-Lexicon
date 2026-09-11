dc36001a6707b127a86f7e52e3b1a3a5cdc8b12d
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-09-11 19:57:58 +0530

    Add solution for Single Number

---
+++ solutions/easy/Single Number.java
@@ -0,0 +1,17 @@
+// Title: Single Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/single-number/
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+            result ^= nums[i]; 
+        }
+
+        return result;
+    }
+class Solution {
+}
+    
+        for (int i = 0; i < nums.length; i++) {
+
