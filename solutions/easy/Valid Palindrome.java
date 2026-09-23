d9a3974957a765b9bfe81eb377c71d0f3195b65c
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-09-23 19:11:17 +0530

    Add solution for Valid Palindrome

--- solutions/easy/Valid Palindrome.java
+++ solutions/easy/Valid Palindrome.java
@@ -3,15 +3,16 @@
             // Language: Java
             // Link: https://leetcode.com/problems/valid-palindrome/
 
+            while(right > left && !Character.isLetterOrDigit(s.charAt
+                right--;
+            }
+            }
+            (right))){
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase
+            (s.charAt(right))){
+                return false;
+            }
         }
-        // 2. Skip non-alphanumeric characters from the right
-        while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-
-    while (left < right) {
-        // 1. Skip non-alphanumeric characters from the left
-        while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-            left++;
-    public boolean isPalindrome(String s) {
-    int left = 0;
-    int right = s.length() - 1;
-class Solution {
+        return true;
+        
+            left++; right--;
